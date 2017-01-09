c58dcb06699ebb38816719e24d5456264d912e77
Krystyn <krystyn@Leeloo.local>
2017-01-09 17:42:27 -0500

    merge changes

--- styleguide-blocks.php
+++ styleguide-blocks.php
@@ -17,6 +17,74 @@
 		</div>
 	</div>
 
+	<!-- image block -->
+	<div class="container container_full">
+		<div class="image_block">
+			<picture>
+				<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
+				<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
+			</picture>
+			<p class="details">Full image block with caption</p>
+		</div>
+	</div>
+
+	<!-- image block x2 -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 image_block">
+				<picture>
+					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
+					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
+				</picture>
+			</div>
+			<div class="col-md-6 image_block">
+				<picture>
+					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
+					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
+				</picture>
+			</div>
+		</div>
+	</div>
+
+	<!-- image block x3 -->
+	<div class="container container_full">
+		<div class="row">
+			<div class="col-md-4 image_block">
+				<picture>
+					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
+					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
+				</picture>
+			</div>
+			<div class="col-md-4 image_block">
+				<picture>
+					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
+					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
+				</picture>
+			</div>
+			<div class="col-md-4 image_block">
+				<picture>
+					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
+					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
+				</picture>
+			</div>
+	</div>
+
+	<!-- image block wrap -->
+	<div class="container container_full">
+		<div class="row">
+			<div class="col-md-6 image_block">
+				<picture>
+					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
+					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
+				</picture>
+			</div>
+			<div class="col-md-6 text_block">
+				<h2>Image Block</h2>
+				<p>Duis aute irure dolor in in voluptate velit esse cillum en dolore ut fugiat nulla pariatur. Excepteur sint proident, sunt in culpa qui officia mollit anim id est laborum. Lorem ipsum dolor sit amet, adipiscing elit do eiusmod tempor incididunt ut labore et dolore consectetur magna. Ut enim ad minim veniam, quis laboris ut aliquip. Duis aute irure dolor in in voluptate velit esse cillum en dolore ut fugiat nulla pariatur. Excepteur sint proident, sunt in culpa qui officia mollit anim id est laborum. Lorem ipsum dolor sit amet, adipiscing elit do eiusmod tempor incididunt ut labore et dolore consectetur magna.</p>
+			</div>
+		</div>
+	</div>
+
 	<!-- code block -->
 	<div class="container">
 		<div class="code_block">
@@ -27,92 +95,6 @@
 			  &nbsp;&nbsp;animation-timing-function: cubic-bezier(0.75, 0.05, 0.85, 0.06);<br>
 				&#125;
 			</code>
-		</div>
-	</div>
-
-	<!-- single image block -->
-	<div class="container container_full">
-		<div class="image_block">
-			<picture>
-				<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
-				<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
-			</picture>
-			<caption>
-				Full Image Block
-			</caption>
-		</div>
-	</div>
-
-	<!-- two image blocks -->
-	<div class="container container_full">
-		<div class="row">
-			<div class="col-md-6 image_block">
-				<picture>
-					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
-					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
-				</picture>
-			</div>
-			<div class="col-md-6 image_block">
-				<picture>
-					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
-					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
-				</picture>
-			</div>
-		</div>
-	</div>
-
-	<!-- three image blocks -->
-	<div class="container container_full">
-		<div class="row">
-			<div class="col-md-3 image_block">
-				<picture>
-					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
-					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
-				</picture>
-			</div>
-			<div class="col-md-3 image_block">
-				<picture>
-					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
-					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
-				</picture>
-			</div>
-			<div class="col-md-3 image_block">
-				<picture>
-					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
-					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
-				</picture>
-			</div>
-	</div>
-
-	<!-- left image block -->
-	<div class="container container_full">
-		<div class="row">
-			<div class="col-md-6 image_block">
-				<picture>
-					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
-					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
-				</picture>
-			</div>
-			<div class="col-md-6 text_block">
-				<h2>Image Block</h2>
-				<p>Duis aute irure dolor in in voluptate velit esse cillum en dolore ut fugiat nulla pariatur. Excepteur sint proident, sunt in culpa qui officia mollit anim id est laborum. Lorem ipsum dolor sit amet, adipiscing elit do eiusmod tempor incididunt ut labore et dolore consectetur magna. Ut enim ad minim veniam, quis laboris ut aliquip. Duis aute irure dolor in in voluptate velit esse cillum en dolore ut fugiat nulla pariatur. Excepteur sint proident, sunt in culpa qui officia mollit anim id est laborum. Lorem ipsum dolor sit amet, adipiscing elit do eiusmod tempor incididunt ut labore et dolore consectetur magna.</p>
-			</div>
-		</div>
-	</div>
-
-	<!-- right image block -->
-	<div class="container container_full">
-		<div class="row">
-			<div class="col-md-6 text_block">
-				<h2>Image Block</h2>
-				<p>Duis aute irure dolor in in voluptate velit esse cillum en dolore ut fugiat nulla pariatur. Excepteur sint proident, sunt in culpa qui officia mollit anim id est laborum. Lorem ipsum dolor sit amet, adipiscing elit do eiusmod tempor incididunt ut labore et dolore consectetur magna. Ut enim ad minim veniam, quis laboris ut aliquip. Duis aute irure dolor in in voluptate velit esse cillum en dolore ut fugiat nulla pariatur. Excepteur sint proident, sunt in culpa qui officia mollit anim id est laborum. Lorem ipsum dolor sit amet, adipiscing elit do eiusmod tempor incididunt ut labore et dolore consectetur magna.</p>
-			</div>
-			<div class="col-md-6 image_block">
-				<picture>
-					<source data-srcset="/images/project-fpo.png, /images/project-fpo-2x.png 2x">
-					<img src="/images/project-fpo.png" alt="Google Androidify" class="img-responsive lazyload">
-				</picture>
-			</div>
 		</div>
 	</div>
 
